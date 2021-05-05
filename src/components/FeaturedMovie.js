5a138ee895418fdefd209c09234cf3dc2749ca48
Juliano Vendramini <JulianoVendramini@outlook.com>
2021-05-05 19:44:26 -0300

    limiting description and create a responsive featured movie

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -9,6 +9,11 @@ export default ({item}) =>{
         genres.push( item.genres[i].name);
     }
 
+    let description = item.overview;
+    if(description.length > 200){
+        description = description.substring(0, 200)+'...'
+    }
+
     return(
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -26,11 +31,11 @@ export default ({item}) =>{
                         <div className = "featured-seasons">{item.number_of_seasons}temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
                     <div className = "featured-description">
-                        {item.overview}
+                        {description}
                     </div>
                     <div className = "featured-buttons">
-                        <a href={`/watch/${item.id}`}>► Assistir</a>
-                        <a href={`/list/add${item.id}`}>+ Minha lista</a>
+                        <a href={`/watch/${item.id}`} className="featured-button-assistir">► Assistir</a>
+                        <a href={`/list/add${item.id}` } className="featured-button-minhalista">+ Minha lista</a>
                     </div>
                     <div className = "featured-genres">
                         <strong>Gêneros: </strong>
