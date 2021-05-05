aa874117a60fd590c2e52447336eb05a581c9596
Juliano Vendramini <JulianoVendramini@outlook.com>
2021-05-05 19:41:42 -0300

    Adding footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+export default () => {
+    return(
+        <footer>
+            Feito pela B7Web<br/>
+            Direitos de Imagem para Netflix<br/>
+            Dados pego do site TheMoviedb.org<br/>
+        </footer>
+    );
+}
