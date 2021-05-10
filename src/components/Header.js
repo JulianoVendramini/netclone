f7da4e58e88f0e3ce27bcf92df79670caa9e41a4
Juliano Vendramini Codognos <JulianoVendramini@outlook.com>
2021-05-10 17:31:14 -0300

    adding user-page

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ export default ({black}) =>{
                 </a>
             </div>
             <div className="header-usuario">
-                <a href="/">
+                <a href="netclone-user-page.vercel.app">
                     <img src = "https://pro2-bar-s3-cdn-cf1.myportfolio.com/dddb0c1b4ab622854dd81280840458d3/98032aebff601c1d993e12a0_rw_600.png?h=8030f4d5734548795c22da59ca72e3e1" alt="usuario"/>
                 </a>
             </div>
